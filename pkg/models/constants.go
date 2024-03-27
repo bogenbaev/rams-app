b9876255d2d3c168d0b14e940575b0e5e6e52924
nugman <bogenbayev.2001@gmail.com>
2024-03-27 22:55:13 +0500

    fix: deleted extra constants...

--- pkg/models/constants.go
+++ pkg/models/constants.go
@@ -1,13 +1,6 @@
 package models
 
 const (
-	Token    = "token"
-	ClientID = "clientId"
-	UserID   = "userId"
-	UserRole = "role"
-)
-
-const (
 	RealPropertyTable = "real_property"
 	UsersTable        = "users"
 )
